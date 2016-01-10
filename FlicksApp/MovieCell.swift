b2cfa1cfebb8003d1977d58a095d6e73a834c933
Corey Salzer <coreysalzer@gmail.com>
2016-01-10 15:13:48 -0500

    Quickstart Video Tutorials

---
+++ FlicksApp/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  FlicksApp
+//
+//  Created by Corey Salzer on 1/10/16.
+//  Copyright © 2016 Corey Salzer. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
